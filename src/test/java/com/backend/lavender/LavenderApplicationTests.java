9de33495eeab69f8a0b40a47e6a3fe6fd71053ac
huy164 <vqh.official@gmail.com>
2022-05-03 09:13:28 +0700

    init project

---
+++ src/test/java/com/backend/lavender/LavenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.lavender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
