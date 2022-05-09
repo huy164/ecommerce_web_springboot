1fe3de83df9dfc6b33f94ee94928307fcb7205fb
huy164 <vqh.official@gmail.com>
2022-05-09 13:20:26 +0700

    apply java regex to validate email

--- src/main/java/com/backend/lavender/controllers/RegisterController.java
+++ src/main/java/com/backend/lavender/controllers/RegisterController.java
@@ -14,8 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-
 import java.util.List;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping(path = "/api/v1/Register")
@@ -28,39 +28,28 @@ public class RegisterController {
     @Autowired
     private SendMailService sendMailService;
 
-    // @GetMapping("")
-    // //this request is: http://localhost:8080/api/v1/Products
-    // List<Customer> getAllProducts() {
-    // return repository.findAll();
-    // }
-    // Get detail product
-    // @GetMapping("/{id}")
-    // //Let's return an object with: data, message, status
-    // ResponseEntity<ResponseObject> findById(@PathVariable Long id) {
-    // Optional<Customer> foundProduct = repository.findById(id);
-    // return foundProduct.isPresent() ?
-    // ResponseEntity.status(HttpStatus.OK).body(
-    // new ResponseObject("ok", "Query product successfully", foundProduct)
-    // //you can replace "ok" with your defined "error code"
-    // ):
-    // ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-    // new ResponseObject("failed", "Cannot find product with id = "+id, "")
-    // );
-    // }
-    // //insert new Product with POST method
-    // //Postman : Raw, JSON
     @PostMapping("/register")
     ResponseEntity<ResponseObject> Register(@RequestBody RegisterDto registerDto) {
 
         Customer newCustomer = new Customer();
         CustomerAccount newCusAcc = new CustomerAccount();
-        BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
+
+        //validate
+        //**REGEX */
+        Pattern pattern = Pattern.compile("@gmail.com",Pattern.CASE_INSENSITIVE);
+        if(!registerDto.getEmail().matches("^[-a-zA-Z0-9._]+")&& !pattern.matcher(registerDto.getEmail()).find()){
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(
+                new ResponseObject("failed","invalid email format", registerDto));
+        }
 
         newCustomer.setName(registerDto.getName());
         newCustomer.setEmail(registerDto.getEmail());
         newCustomer.setPhoneNumber(registerDto.getPhoneNumber());
         newCustomer.setDob(registerDto.getDob());
+
+        
 
         List<Customer> foundCustomer = CustomerRepository.findByEmail(newCustomer.getEmail().trim());
         if (foundCustomer.size() > 0) {
@@ -76,57 +65,29 @@ public class RegisterController {
 
         Customer savedCustomer = CustomerRepository.save(newCustomer);
 
-        
+
+
         newCusAcc.setUsername(registerDto.getUsername());
-        String encodedPassword=encoder.encode(registerDto.getPassword());
+        String encodedPassword = encoder.encode(registerDto.getPassword());
         newCusAcc.setPassword(encodedPassword);
         newCusAcc.setCustomer(savedCustomer);
 
-        List<CustomerAccount> foundTkkh= customerAccountRepository.findByUsername(newCusAcc.getUsername().trim());
+
+        List<CustomerAccount> foundTkkh = customerAccountRepository.findByUsername(newCusAcc.getUsername().trim());
         if (foundTkkh.size() > 0) {
             return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(
                     new ResponseObject("failed", "username taken", ""));
         }
 
-        CustomerAccount savedCusAcc=customerAccountRepository.save(newCusAcc);
+        CustomerAccount savedCustomerAccount = customerAccountRepository.save(newCusAcc);
 
         String htmlemail = "Bạn đã đăng ký tài khoản trên Lavender";
-        
+
         sendMailService.sendSimpleMessage(newCustomer.getEmail(), "xac nhan tai khoan", htmlemail);
         return ResponseEntity.status(HttpStatus.OK).body(
-                new ResponseObject("ok", "Insert customer successfully", savedCustomer.toString().concat(savedCusAcc.toString())));
-    
-        }
-    // update, upsert = update if found, otherwise insert
-    // @PutMapping("/{id}")
-    // ResponseEntity<ResponseObject> updateProduct(@RequestBody Customer
-    // newProduct, @PathVariable Long id) {
-    // Customer updatedProduct = repository.findById(id)
-    // .map(product -> {
-    // product.setProductName(newProduct.getProductName());
-    // product.setYear(newProduct.getYear());
-    // product.setPrice(newProduct.getPrice());
-    // return repository.save(product);
-    // }).orElseGet(() -> {
-    // newProduct.setId(id);
-    // return repository.save(newProduct);
-    // });
-    // return ResponseEntity.status(HttpStatus.OK).body(
-    // new ResponseObject("ok", "Update Product successfully", updatedProduct)
-    // );
-    // }
-    // //Delete a Product => DELETE method
-    // @DeleteMapping("/{id}")
-    // ResponseEntity<ResponseObject> deleteProduct(@PathVariable Long id) {
-    // boolean exists = repository.existsById(id);
-    // if(exists) {
-    // repository.deleteById(id);
-    // return ResponseEntity.status(HttpStatus.OK).body(
-    // new ResponseObject("ok", "Delete product successfully", "")
-    // );
-    // }
-    // return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-    // new ResponseObject("failed", "Cannot find product to delete", "")
-    // );
-    // }
+                new ResponseObject("ok", "Insert customer successfully",
+                        savedCustomer.toString().concat(savedCustomerAccount.toString())));
+
+    }
+
 }
--- src/main/java/com/backend/lavender/models/ResponseObject.java
+++ src/main/java/com/backend/lavender/models/ResponseObject.java
@@ -11,77 +11,11 @@ public class ResponseObject {
   private String message;
   private Object data;
 
-  public ResponseObject() {
-  }
-
   public ResponseObject(String status, String message, Object data) {
     this.status = status;
     this.message = message;
     this.data = data;
   }
 
-  public String getStatus() {
-    return this.status;
-  }
-
-  public void setStatus(String status) {
-    this.status = status;
-  }
-
-  public String getMessage() {
-    return this.message;
-  }
-
-  public void setMessage(String message) {
-    this.message = message;
-  }
-
-  public Object getData() {
-    return this.data;
-  }
-
-  public void setData(Object data) {
-    this.data = data;
-  }
-
-  public ResponseObject status(String status) {
-    setStatus(status);
-    return this;
-  }
-
-  public ResponseObject message(String message) {
-    setMessage(message);
-    return this;
-  }
-
-  public ResponseObject data(Object data) {
-    setData(data);
-    return this;
-  }
-
-  @Override
-    public boolean equals(Object o) {
-        if (o == this)
-            return true;
-        if (!(o instanceof ResponseObject)) {
-            return false;
-        }
-        ResponseObject responseObject = (ResponseObject) o;
-        return Objects.equals(status, responseObject.status) && Objects.equals(message, responseObject.message) && Objects.equals(data, responseObject.data);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(status, message, data);
-  }
-
-  @Override
-  public String toString() {
-    return "{" +
-      " status='" + getStatus() + "'" +
-      ", message='" + getMessage() + "'" +
-      ", data='" + getData() + "'" +
-      "}";
-  }
-
+ 
 }
